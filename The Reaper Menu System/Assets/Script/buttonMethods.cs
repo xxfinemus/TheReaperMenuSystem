09e70067d0f6def5016e4d75360844c1517ba8d6
DESKTOP-B0O0GUK\Thoma <thomas91hansen@gmail.com>
2016-09-09 15:54:20 +0200

    added 2 Scenes (canvas and GUI)

---
+++ The Reaper Menu System/Assets/Script/buttonMethods.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class buttonMethods : MonoBehaviour {
+    public Image visitcart;
+    Animator anim;
+
+    void Start()
+    {
+        
+    }
+	// Use this for initialization
+    public void continueGame()
+    {
+       
+    }
+}
---
+++ The Reaper Menu System/Assets/Script/graphicsScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class graphicsScript : MonoBehaviour {
+    public Canvas deskCanvas;
+    float height;
+    float width;
+	// Use this for initialization
+	void Start () {
+        deskCanvas.GetComponent<Canvas>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setRessolution()
+    {
+        height = Camera.main.orthographicSize * 2;
+        width = height * Screen.width / Screen.height; // basically height * screen aspect ratio
+        int length = gameObject.transform.childCount;
+        for (int i = 0; i <length; i++)
+			{
+                gameObject.transform.GetChild(i).localScale = Vector3.one * height / 20f;
+			}
+        
+    }
+}
